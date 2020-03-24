3e37bd2a8398cb6915a2738cf2b17bcd2547cea7
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-23 21:54:00 -0700

    additional functionality

--- rawWebComponents/to-do-app.js
+++ rawWebComponents/to-do-app.js
@@ -31,8 +31,47 @@ class TodoApp extends HTMLElement {
     super();
     this._shadowRoot = this.attachShadow({ 'mode': 'open' });
     this._shadowRoot.appendChild(template.content.cloneNode(true));
+
     this.$todoList = this._shadowRoot.querySelector('ul');
+    this.$input = this._shadowRoot.querySelector('input');
+
+    this.$submitButton = this._shadowRoot.querySelector('button');
+    this.$submitButton.addEventListener('click', this._addTodo.bind(this));
+  }
+
+  _addTodo() {
+    if (this.$input.value.length > 0) {
+      console.log('clicked')
+      this._todos.push({ text: this.$input.value, checked: false })
+      this._renderTodoList();
+      this.$input.value = '';
+    }
+
+  }
+
+  _renderTodoList() {
+    this.$todoList.innerHTML = '';
+
+    this._todos.forEach((todo, index) => {
+      let $todoItem = document.createElement('div');
+      $todoItem.innerHTML = todo.text;
+      this.$todoList.appendChild($todoItem);
+    });
+  }
+
+  set todos(value) {
+    this._todos = value;
+    this._renderTodoList();
+  }
+
+  get todos() {
+    return this._todos;
   }
 }
+
+document.querySelector('to-do-app')._todos = [
+  { text: "Make a to-do list", checked: false },
+  { text: "Finish blog post", checked: false }
+];
 
 window.customElements.define('to-do-app', TodoApp);
