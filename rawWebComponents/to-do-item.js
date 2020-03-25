7e42b9ce6ed78b2d9ddeaac0c1de5d107a3b5e29
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-25 06:42:53 -0700

    added check for attribute update

--- rawWebComponents/to-do-item.js
+++ rawWebComponents/to-do-item.js
@@ -64,11 +64,18 @@ class TodoItem extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ['text'];
+    return ['text', 'checked'];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    this._text = newValue;
+    switch (name) {
+      case 'text':
+        this._text = newValue;
+        break;
+      case 'checked':
+        this._checked = this.hasAttribute('checked');
+        break;
+    }
   }
 }
 
