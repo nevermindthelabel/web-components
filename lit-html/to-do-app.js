2d0e997a6f1436771098afef3938df6eb68ea157
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-28 19:48:02 -0700

    updated todo component

--- lit-html/to-do-app.js
+++ lit-html/to-do-app.js
@@ -1,4 +1,4 @@
-import { html, render }from 'lit-html';
+import { html, render }from '../node_modules/lit-html/lit-html.js';
 
 class TodoApp extends HTMLElement {
   constructor() {
@@ -11,6 +11,32 @@ class TodoApp extends HTMLElement {
       { text: 'Supercharge your web components', checked: false },
       { text: 'Attributes, properties, and events', checked: false },
       { text: 'Wrapping up', checked: false }
-    ]
+    ];
+
+    render(this.template(), this._shadowRoot, { eventContext: this });
+  }
+
+  template() {
+    return html `
+    <style>
+      :host {
+        display: block;
+        font-family: sans-serif;
+        text-align: center;
+      }
+      button {
+        border: none;
+        cursor: pointer;
+        background-color: transparent;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+    </style>
+    ${this.todos.forEach(todo => console.log(todo.text))}
+    `
   }
 }
+
+window.customElements.define('to-do-app', TodoApp);
